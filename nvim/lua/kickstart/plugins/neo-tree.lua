e9ac243e7836ae53f724d86b881eeba1e497bf7a
Rohit Roy <rohit31roy97@gmail.com>
2024-08-10 06:45:28 -0400

    Added in configuration files for neovim

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,37 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        position = 'left',
+        width = 20,
+        mappings = {
+          ['\\'] = 'close_window',
+          ['D'] = 'delete',
+          ['<bs>'] = 'navigate_up',
+          ['.'] = 'set_root',
+          ['o'] = { 'show_help', nowait = false, config = { title = 'Order by', prefix_key = 'o' } },
+          ['oc'] = { 'order_by_created', nowait = false },
+          ['od'] = { 'order_by_diagnostics', nowait = false },
+          ['om'] = { 'order_by_modified', nowait = false },
+          ['on'] = { 'order_by_name', nowait = false },
+          ['os'] = { 'order_by_size', nowait = false },
+          ['ot'] = { 'order_by_type', nowait = false },
+        },
+      },
+    },
+  },
+}
